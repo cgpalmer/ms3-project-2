6c17e4bf3846549e78718dcb6a7a2e9f916a0e2f
Chris Palmer <cgpalmer91@gmail.com>
2020-06-09 21:43:27 +0000

    Commenting out code as I have changed the format of the website.

--- app.py
+++ app.py
@@ -40,50 +40,50 @@ def insert_report():
     return redirect(url_for('homepage'))
 
 
-@app.route('/insert_user', methods=['POST'])
-def insert_user():
-    user = mongo.db.user_credentials
-    user.insert_one(request.form.to_dict())
-    return redirect(url_for('login_page'))
+# @app.route('/insert_user', methods=['POST'])
+# def insert_user():
+#     user = mongo.db.user_credentials
+#     user.insert_one(request.form.to_dict())
+#     return redirect(url_for('login_page'))
 
 
-@app.route('/signup_page')
-def signup_page():
-    return render_template("signup.html")
+# @app.route('/signup_page')
+# def signup_page():
+#     return render_template("signup.html")
 
-@app.route('/login_page')
-def login_page():
-    return render_template("login.html")
-
-
-@app.route('/enter_username', methods=['POST'])
-def enter_username():
-    login_username = request.form['login_username']
-    login_password = request.form['login_password']
-    user = mongo.db.user_credentials.find({'username': login_username, 'user_password': login_password})
-    if user.count() > 0:
-        session['username'] = login_username
-        return redirect(url_for('user_dash'), user=mongo.db.user_credentials.find({'username': login_username))
-    else:
-        return redirect(url_for('login_page'))
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.pop('username', None)
-    print(session['username'])
-    return redirect(url_for('login_page'))
+# @app.route('/login_page')
+# def login_page():
+#     return render_template("login.html")
 
 
+# @app.route('/enter_username', methods=['POST'])
+# def enter_username():
+#     login_username = request.form['login_username']
+#     login_password = request.form['login_password']
+#     user = mongo.db.user_credentials.find({'username': login_username, 'user_password': login_password})
+#     if user.count() > 0:
+#         session['username'] = login_username
+#         return redirect(url_for('user_dash'))
+#     else:
+#         return redirect(url_for('login_page'))
 
+# @app.route('/logout', methods=['POST'])
+# def logout():
+#     session.pop('username', None)
+#     print(session['username'])
+#     return redirect(url_for('login_page'))
 
 
 
-@app.route('/user_dash')
-def user_dash():
-    if session['username'] is not None:
-        return render_template("user_dash.html", user=mongo.db.user_credentials.find())
-    else:
-        return redirect(url_for('login_page'))
+
+
+
+# @app.route('/user_dash')
+# def user_dash():
+#     if session['username'] is not None:
+#         return render_template("user_dash.html", user=mongo.db.user_credentials.find())
+#     else:
+#         return redirect(url_for('login_page'))
 
 
 if __name__ == '__main__':
