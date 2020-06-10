23251b4085a2f412b0331927cf6e7646a70d20fd
Chris Palmer <cgpalmer91@gmail.com>
2020-06-10 19:34:20 +0000

    Removing extra code to focus on a single task.

--- app.py
+++ app.py
@@ -50,55 +50,9 @@ def edit_report():
 
 @app.route('/delete_report/<report_id>')
 def delete_report(report_id):
-    #Trigger a modal of some kind where the user has to input the correct email.
+    # Trigger a modal of some kind where the user has to input the correct email.
     mongo.db.report.remove({'_id': ObjectId(report_id)})
     return redirect(url_for('get_report'))
-
-
-# @app.route('/insert_user', methods=['POST'])
-# def insert_user():
-#     user = mongo.db.user_credentials
-#     user.insert_one(request.form.to_dict())
-#     return redirect(url_for('login_page'))
-
-
-# @app.route('/signup_page')
-# def signup_page():
-#     return render_template("signup.html")
-
-# @app.route('/login_page')
-# def login_page():
-#     return render_template("login.html")
-
-
-# @app.route('/enter_username', methods=['POST'])
-# def enter_username():
-#     login_username = request.form['login_username']
-#     login_password = request.form['login_password']
-#     user = mongo.db.user_credentials.find({'username': login_username, 'user_password': login_password})
-#     if user.count() > 0:
-#         session['username'] = login_username
-#         return redirect(url_for('user_dash'))
-#     else:
-#         return redirect(url_for('login_page'))
-
-# @app.route('/logout', methods=['POST'])
-# def logout():
-#     session.pop('username', None)
-#     print(session['username'])
-#     return redirect(url_for('login_page'))
-
-
-
-
-
-
-# @app.route('/user_dash')
-# def user_dash():
-#     if session['username'] is not None:
-#         return render_template("user_dash.html", user=mongo.db.user_credentials.find())
-#     else:
-#         return redirect(url_for('login_page'))
 
 
 if __name__ == '__main__':
