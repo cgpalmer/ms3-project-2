75f26bc967e0505a17f527f7081bb007a744e255
Chris Palmer <cgpalmer91@gmail.com>
2020-06-11 23:10:36 +0000

    Writing in a report to update the main fields for now. Also passing through the report id of a report to the user_modify.2

--- app.py
+++ app.py
@@ -40,12 +40,23 @@ def insert_report():
     return redirect(url_for('homepage'))
 
 
-@app.route('/user_modify')
-def user_modify():
-    return render_template('user_modify.html')
+@app.route('/user_modify/<report_id>')
+def user_modify(report_id):
+    the_report = mongo.db.report.find_one({"_id": ObjectId(report_id)})
+    return render_template('user_modify.html', report=the_report)
 
-@app.route('/edit_report')
-def edit_report():
+@app.route('/edit_report/<report_id>', methods=["POST"])
+def edit_report(report_id):
+    report = mongo.db.report
+    report.update({'_id': ObjectId(report_id)},
+                  {
+                  'username': request.form.get('username'),
+                  'category_name': request.form.get('category_name'),
+                  'sub_category': request.form.get('sub_category'),
+                  'insert_description': request.form.get('insert_description'),
+                  'location': request.form.get('location'),
+                  'is_urgent':request.form.get('is_urgent')
+                  })
     return redirect(url_for('get_report'))
 
 @app.route('/delete_report/<report_id>')
