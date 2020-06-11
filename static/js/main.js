e80f05ca8792d5317a022105df9d72c194a254bb
Chris Palmer <cgpalmer91@gmail.com>
2020-06-11 00:10:10 +0000

    Adding an option to display a form element to add in a category.

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,7 @@
         if (val == "Xenophobia") {
             $("#subCategoryChoices").css("display", "block")
         }
+        else if(val == "Xenophobia"){
+            $("#addCategoryChoice").css("display", "block")
+        }
  });
