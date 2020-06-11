143111cb378a92cb09e0e6ed51f3a20c1e5914f6
Chris Palmer <cgpalmer91@gmail.com>
2020-06-11 12:11:53 +0000

    FB - Other sub_category didn't disappear when categoryw as changed from xenophobia to other categories.

--- static/js/main.js
+++ static/js/main.js
@@ -3,14 +3,17 @@
         if (val == "Xenophobia") {
             $("#subCategoryChoices").css("display", "block")
             $("#addCategoryChoice").css("display", "none")
+            $("#addSubCategoryChoice").css("display", "none")
         }
         else if(val == "Other"){
             $("#addCategoryChoice").css("display", "block")
             $("#subCategoryChoices").css("display", "none")
+            $("#addSubCategoryChoice").css("display", "none")
         }
         else{
             $("#subCategoryChoices").css("display", "none")
             $("#addCategoryChoice").css("display", "none")
+            $("#addSubCategoryChoice").css("display", "none")
         }
  });
 
