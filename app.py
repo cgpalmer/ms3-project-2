6705eeb764034adcc5b147a03b2f0538fc837a6a
Chris Palmer <cgpalmer91@gmail.com>
2020-06-30 05:46:00 +0000

    Duplicating the retrieve parameter route to handle reports with filters on.

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def retrieving_report():
         value = request.form["search_value{}".format(x)]
         print(value)
         values.append(value)
-        
+
         # Return values
     if number_of_fields == 2:
         print("if 2")
@@ -111,6 +111,37 @@ def retrieving_report():
     return render_template("results_display.html", report=the_report)
 
 
+# This submits the final report and returns the reports
+@app.route('/retrieving_report_with_filters', methods=["POST"])
+def retrieving_report_with_filters():
+    global extra_parameters
+    choices = []
+    values = []
+    fields = request.form["number_of_fields"]
+    number_of_fields = int(fields)
+    print(str(number_of_fields))
+    for x in range(int(number_of_fields)):
+        print(x)
+        key = request.form["search_choice{}".format(x)]
+        print(key)
+        choices.append(key)
+        value = request.form["search_value{}".format(x)]
+        print(value)
+        values.append(value)
+
+        # Return values
+    if number_of_fields == 2:
+        print("if 2")
+        the_report = mongo.db.report.find( { "$and": [ { choices[0]:values[0] }, { choices[1] : values[1]} ] } )
+    elif number_of_fields  == 3:
+        the_report = mongo.db.report.find( { "$and": [ { choices[0]:values[0] }, { choices[1] : values[1]}, { choices[2] : values[2]} ] } )
+    elif number_of_fields  == 4:
+        the_report = mongo.db.report.find( { "$and": [ { choices[0]:values[0] }, { choices[1] : values[1]}, { choices[2] : values[2]}, { choices[3] : values[3]} ] } )
+    elif number_of_fields  == 5:
+        the_report = mongo.db.report.find( { "$and": [ { choices[0]:values[0] }, { choices[1] : values[1]}, { choices[2] : values[2]}, { choices[3] : values[3]}, { choices[4] : values[4]} ] } )
+    else:
+        the_report = mongo.db.report.find( {choices[0]: values[0]})
+    return render_template("results_display.html", report=the_report)
 
 
 
