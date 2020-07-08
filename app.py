ca454be5f01c252d11c0f0364daa9cb381227c85
Chris Palmer <cgpalmer91@gmail.com>
2020-07-08 19:46:48 +0000

    Using the variable 'stored_salt' in the process of validating the input password on the check password route.

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def check_password():
     hash_login_password = hashlib.pbkdf2_hmac(
     'sha256', # The hash digest algorithm for HMAC
     login_password.encode('utf-8'), # Convert the password to bytes
-    salt, # Provide the salt
+    stored_salt, # Provide the salt
     100000, # It is recommended to use at least 100,000 iterations of SHA-256 
     dklen=128 # Get a 128 byte key
     )
