786704f41079a1ff4f6293dcfebbca1f7de037be
Chris Palmer <cgpalmer91@gmail.com>
2020-07-16 20:01:53 +0000

    Adding in a function so that when the link is pressed it changes the id.

--- static/js/main.js
+++ static/js/main.js
@@ -1,98 +1,108 @@
- $("#category").change(function() {
-        var val = $(this).val();
-        if (val == "Xenophobia") {
-            $("#subCategoryChoices").css("display", "block");
-            $("#addCategoryChoice, #addSubCategoryChoice").css("display", "none")
-        }
-        else if(val == "Other"){
-            $("#addCategoryChoice").css("display", "block")
-            $("#subCategoryChoices, #addSubCategoryChoice").css("display", "none")
-        }
-        else{
-            $("#subCategoryChoices, #addCategoryChoice, #addSubCategoryChoice").css("display", "none");
-        }
- });
+//  $("#category").change(function() {
+//         var val = $(this).val();
+//         if (val == "Xenophobia") {
+//             $("#subCategoryChoices").css("display", "block");
+//             $("#addCategoryChoice, #addSubCategoryChoice").css("display", "none")
+//         }
+//         else if(val == "Other"){
+//             $("#addCategoryChoice").css("display", "block")
+//             $("#subCategoryChoices, #addSubCategoryChoice").css("display", "none")
+//         }
+//         else{
+//             $("#subCategoryChoices, #addCategoryChoice, #addSubCategoryChoice").css("display", "none");
+//         }
+//  });
 
- $("#sub_category").change(function() {
-        var val = $(this).val();
-        if (val == "Other") {
-            $("#addSubCategoryChoice").css("display", "block")
-        }
-        else{
-            $("#addSubCategoryChoice").css("display", "none")
-        }
- });
-
-
-  $("#search_type").change(function() {
-        var val = $(this).val();
-        $(".searchValue").addClass("hidden");
-        if (val == "email") {
-            $("#search_email").removeClass("hidden");
-        }
-        else if (val == "username") {
-            $("#search_username").removeClass("hidden");
-        }
-
- });
+//  $("#sub_category").change(function() {
+//         var val = $(this).val();
+//         if (val == "Other") {
+//             $("#addSubCategoryChoice").css("display", "block")
+//         }
+//         else{
+//             $("#addSubCategoryChoice").css("display", "none")
+//         }
+//  });
 
 
-  $("#basic").click(function(){;
-      $("#orSearch").toggle();
-  });
+//   $("#search_type").change(function() {
+//         var val = $(this).val();
+//         $(".searchValue").addClass("hidden");
+//         if (val == "email") {
+//             $("#search_email").removeClass("hidden");
+//         }
+//         else if (val == "username") {
+//             $("#search_username").removeClass("hidden");
+//         }
 
-    $("#filtered").click(function(){;
-      $("#filteredSearch").toggle();
-  });
+//  });
+
+
+//   $("#basic").click(function(){;
+//       $("#orSearch").toggle();
+//   });
+
+//     $("#filtered").click(function(){;
+//       $("#filteredSearch").toggle();
+//   });
 
 
 
-//  Signup page
+// //  Signup page
 
-// Temporarily seeing passwords.
+// // Temporarily seeing passwords.
 
-$("#seePassword").click(function(){
-         var seePassword = document.getElementById('password');
-        seePassword.type = "text";
-    var passwordTimeout = setTimeout(passwordReveal, 2000);
-    function passwordReveal() {
-        seePassword.type = "password";
-    }
-    clearTimeout();
+// $("#seePassword").click(function(){
+//          var seePassword = document.getElementById('password');
+//         seePassword.type = "text";
+//     var passwordTimeout = setTimeout(passwordReveal, 2000);
+//     function passwordReveal() {
+//         seePassword.type = "password";
+//     }
+//     clearTimeout();
     
-})
+// })
 
 
 
-//   Matching passwords
+// //   Matching passwords
 
 
 
-// There is a console error on this. 
+// // There is a console error on this. 
 
 
-// In here add the code to put a tick next to the password box once it is a certain length.
+// // In here add the code to put a tick next to the password box once it is a certain length.
 
 
-     // Live validating the password
-    password = document.getElementById('password')
-    password.onkeyup = function(){
-    var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    var input = document.getElementById('password')
-        if(input.value.match(passw))
-            {
-            $("#validatedPassword").removeClass('hidden');
-            }
-        else
-            {
-            $("#validatedPassword").addClass('hidden');
-            }
-    }
+//      // Live validating the password
+//     password = document.getElementById('password')
+//     password.onkeyup = function(){
+//     var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+//     var input = document.getElementById('password')
+//         if(input.value.match(passw))
+//             {
+//             $("#validatedPassword").removeClass('hidden');
+//             }
+//         else
+//             {
+//             $("#validatedPassword").addClass('hidden');
+//             }
+//     }
 
 
 
-    // Unhiding the confirm delete button
+//     // Unhiding the confirm delete button
 
-    $("#deleteAccount").click(function(){
-        $("#confirmDeleteUser").removeClass("hidden");
-    });
+//     $("#deleteAccount").click(function(){
+//         $("#confirmDeleteUser").removeClass("hidden");
+//     });
+
+
+
+// Animations
+// Search page
+
+$("#searchLink").click(function(){ 
+    alert("reached")
+    $('#basicSearchForm'). attr('action', ''); 
+});
