d035f27a43d339a31e3e5ec41568d24586a1c736
Chris Palmer <cgpalmer91@gmail.com>
2020-07-25 08:19:34 +0000

    Re sorting all the code after trying to upgrade to materialize 1.00.2

--- static/js/main.js
+++ static/js/main.js
@@ -126,17 +126,10 @@ $("#searchLinkCompare").click(function(){
     $('select').formSelect();
   });
 
-//    $(document).ready(function(){
-//     $('.datepicker').datepicker();
-//   });
- $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15, // Creates a dropdown of 15 years to control year,
-            today: 'Today',
-            clear: 'Clear',
-            close: 'Ok',
-            closeOnSelect: false // Close upon selecting a date,
-        });
+   $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+       
 
  $(document).ready(function(){
     $('.sidenav').sidenav();
