bcbea76bfb7ce1eed522c3d33e319b0568fb0804
Chris Palmer <cgpalmer91@gmail.com>
2020-08-04 18:09:58 +0000

    Used the collapsible headers to change which timeFrame was shown forsearching the db.

--- static/js/main.js
+++ static/js/main.js
@@ -1,56 +1,38 @@
 //  Js to narrow down user choice when submitting forms.
- 
+let collapsibleChoice;
+let timeFrame;
+
+$("#categoryCollapsible").click(function(){
+    collapsibleChoice = "category"
+    timeFrame = ".catTimeFrameDisplay"
+    console.log(timeFrame)
+ });
+
+$("#locationCollapsible").click(function(){
+    collapsibleChoice = "location"
+ });
+
+$("#reportedCollapsible").click(function(){
+    collapsibleChoice = "reported"
+ });
+
+ $("#allCollapsible").click(function(){
+    collapsibleChoice = "all"
+    
+ });
+
  $(".timeFrameChoice").change(function() {
+     
      var val = $(this).val();
      if (val == "Yes") {
-            $(".timeFrame").removeClass('hidden');
-        }
+            $(timeFrame).removeClass('hidden');
+            console.log(timeFrame)
+         }
     else {
-            $(".timeFrame").addClass('hidden');
+            $(timeFrame).addClass('hidden');
         }
  });
 
-
-
- $("#category").change(function() {
-        var val = $(this).val();
-        if (val == "Xenophobia") {
-            $("#subCategoryChoices").css("display", "block");
-            $("#addCategoryChoice, #addSubCategoryChoice").css("display", "none")
-        }
-        else if(val == "Other"){
-            $("#addCategoryChoice").css("display", "block")
-            $("#subCategoryChoices, #addSubCategoryChoice").css("display", "none")
-        }
-        else{
-            $("#subCategoryChoices, #addCategoryChoice, #addSubCategoryChoice").css("display", "none");
-        }
- });
-
- 
-
- $("#sub_category").change(function() {
-        var val = $(this).val();
-        if (val == "Other") {
-            $("#addSubCategoryChoice").css("display", "block")
-        }
-        else{
-            $("#addSubCategoryChoice").css("display", "none")
-        }
- });
-
-
-  $("#search_type").change(function() {
-        var val = $(this).val();
-        $(".searchValue").addClass("hidden");
-        if (val == "email") {
-            $("#search_email").removeClass("hidden");
-        }
-        else if (val == "username") {
-            $("#search_username").removeClass("hidden");
-        }
-
- });
 
 
 
