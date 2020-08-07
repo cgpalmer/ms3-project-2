21af015f895ba460e80e08d53de341998402a83b
Chris Palmer <cgpalmer91@gmail.com>
2020-08-07 15:35:02 +0000

    Adding in a conditional in the check_password to return the user to the login page if they do not use a valid email.

--- app.py
+++ app.py
@@ -214,20 +214,24 @@ def check_password():
     login_password = request.form['login_password']
     print(login_password)
     current_user = mongo.db.user_credentials.find_one({"user_email": login_email})
-    stored_password = get_user_password(current_user)
-    stored_salt = get_user_password_salt(current_user)
-    login_name = get_user_login_name(current_user)
-    hash_login_password = hash_a_password_to_check_it_is_correct(stored_salt, login_password)
-    print(hash_login_password)
-    if stored_password == hash_login_password:
-        print("match")
-        session["email"] = login_email
-        username = session.get("email")
-        session["name"] = login_name              
-        return redirect(url_for('dashboard'))
+    if current_user == None:
+        flash("Sorry, this email hasn't been registered with us yet.")
+        return redirect(url_for('login'))
     else:
-        print("no match")
-        return render_template('login.html')
+        stored_password = get_user_password(current_user)
+        stored_salt = get_user_password_salt(current_user)
+        login_name = get_user_login_name(current_user)
+        hash_login_password = hash_a_password_to_check_it_is_correct(stored_salt, login_password)
+        print(hash_login_password)
+        if stored_password == hash_login_password:
+            print("match")
+            session["email"] = login_email
+            username = session.get("email")
+            session["name"] = login_name              
+            return redirect(url_for('dashboard'))
+        else:
+            print("no match")
+            return render_template('login.html')
 
 
 ############################################################
